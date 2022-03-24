381ec4f1636a4657d7b244c18c3a10fef4642791
Gustavo <batistag395@gmail.com>
2022-03-24 00:10:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tabuada_For
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //for(int numero = 0; numero <=10; numero++)
+            //{
+            //    for(int numero_ = 0; numero_ <= 10;numero_++)
+            //    {
+            //        Console.WriteLine(numero + " * " + numero_ + " = " + numero * numero_);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+            for (int numero = 0; numero <= 10; numero++)
+            {
+                int resultado = numero * 7;
+                Console.WriteLine(numero + " X " + " 7 = " + resultado);
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
